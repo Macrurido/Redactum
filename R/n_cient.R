007f1f2cf5e82993e4e7a515ba5f6b76e427555f
Macrurido <hugo.aguirre@imipas.gob.mx>
2026-02-27 20:11:37 -0700

    Adding n_cient()

--- R/n_cient.R
+++ R/n_cient.R
@@ -6,6 +6,8 @@
 #'
 #' Within the function, a condition is established. If the scientific name refers to a species that is not specified under a particular genus, such as Hydrolagus sp., or to multiple species within a genus, like Hydrolagus spp., the name will not be abbreviated. However, if the scientific name designates a specific species, it will be modified; for instance, Hydrolagus melanophasma will be abbreviated to H. melanophasma.
 #'
+#' The function operates correctly with or without periods in abbreviations, such as "sp" or "sp."
+#'
 #' @param name A character string containing the scientific name.
 #'
 #' @returns A character string containing the abbreviated scientific name.
@@ -17,11 +19,11 @@
 #' name <- "Hydrolagus melanophasma"
 #' n_cient(name)
 #'
-#' # For unnamed species in a genus
-#' name <- "Hydrolagus sp."
+#' ## Example: Using the abbreviation "sp" without a period.
+#' name <- "Hydrolagus sp"
 #' n_cient(name)
 #'
-#' # For several unnamed species within a particular genus.
+#' ## Example: Using the abbreviation "spp." with a period.
 #' name <- "Hydrolagus spp."
 #' n_cient(name)
 #'
@@ -30,7 +32,8 @@ n_cient <- function(name){
   name_split <- strsplit(name, split = " ")
   genus <- substr(name, 1, 1)
   species <- name_split[[1]][2]
-  new_name <- ifelse(species == "sp." | species == "spp.",
+  new_name <- ifelse(species == "sp." | species == "spp." |
+                       species == "sp" | species == "spp",
                      name,
                      paste(genus, ". ", species, sep = ""))
   print(new_name)
